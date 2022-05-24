919131b44f82e5dcf9e8954059a58d8146f89985
Artur Todeschini <artodeschini@gmail.com>
2022-05-24 08:37:10 -0300

    add teste calculadora com assertj

--- src/main/java/org/todeschini/Calculadora.java
+++ src/main/java/org/todeschini/Calculadora.java
@@ -2,5 +2,26 @@ package org.todeschini;
 
 public class Calculadora {
 
+    public int somar(int n1, int n2) {
+        if (n1 < 0 || n2 < 0) {
+            throw new RuntimeException("nao deve somar numeros negativos");
+        }
+        return n1 + n2;
+    }
 
+    public int subtrair(int n1, int n2) {
+        return n1 - n2;
+    }
+
+    public int multiplicar(int n1, int n2) {
+        return n1 * n2;
+    }
+
+
+    public int dividir(int n1, int n2) {
+        if (n2 == 0) {
+            throw new RuntimeException("divisao nao pode ser por zero");
+        }
+        return n1 / n2;
+    }
 }
--- src/test/java/org/todeschini/CalculadoraTest.java
+++ src/test/java/org/todeschini/CalculadoraTest.java
@@ -1,7 +1,108 @@
 package org.todeschini;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CalculadoraTest {
+
+    private Calculadora calculadora;
+
+    @BeforeEach
+    void setUp() {
+        calculadora = new Calculadora();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculadora = null;
+    }
+
+    @Test
+    void deveSomar() {
+        //given
+        int n1 = 10, n2 = 5, resultado;
+
+        //when
+        resultado = calculadora.somar(n1, n2);
+
+        //then
+        assertThat(resultado).isEqualTo(15);
+    }
+
+    @Test
+    void naoDeveSomarNumerosNegativos() {
+        //given
+        int n1 = 10, n2 = -5, resultado;
+
+        //when // then
+        //assertThat(calculadora.somar(n1, n2)).th;
+        assertThatThrownBy(() -> {
+            calculadora.somar(n1, n2);
+        }).isInstanceOf(RuntimeException.class).hasMessage("nao deve somar numeros negativos").hasMessageContaining("negativos");
+    }
+
+    @Test
+    void naoDeveSomarNumerosNegativosOutro() {
+        //given
+        int n1 = 10, n2 = -5, resultado;
+
+        //when // then
+        //assertThat(calculadora.somar(n1, n2)).th;
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+            calculadora.somar(n1, n2);
+        }).withMessageContaining("negativos");
+    }
+
+    @Test
+    void deveSubtrair() {
+        //given
+        int n1 = 10, n2 = 5, resultado;
+
+        //when
+        resultado = calculadora.subtrair(n1, n2);
+
+        //then
+        assertThat(resultado).isEqualTo(5);
+    }
+
+    @Test
+    void deveMultiplicar() {
+        //given
+        int n1 = 10, n2 = 5, resultado;
+
+        //when
+        resultado = calculadora.multiplicar(n1, n2);
+
+        //then
+        assertThat(resultado).isEqualTo(50);
+    }
+
+    @Test
+    void deveDividir() {
+        //given
+        int n1 = 10, n2 = 5, resultado;
+
+        //when
+        resultado = calculadora.dividir(n1, n2);
+
+        //then
+        assertThat(resultado).isEqualTo(2);
+    }
+
+    @Test
+    void naoDeveDividirPorZero() {
+        //given
+        int n1 = 10, n2 = 0;
+
+        //when // then
+        assertThatThrownBy(() -> {
+            calculadora.dividir(n1, n2);
+        }).isInstanceOf(RuntimeException.class).hasMessage("divisao nao pode ser por zero").hasMessageContaining("zero");
+    }
 
 }
